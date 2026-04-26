3a2c6ff08549f21f56b56a4af8ccce387710e852
Younsoo Park <danielpark605@gmail.com>
2026-04-26 03:00:21 -0400

    Word wrap_8

--- src/utils/textWrapNormalize.ts
+++ src/utils/textWrapNormalize.ts
@@ -1,7 +1,14 @@
 export function repairMidWordBreaks(text: string): string {
-  return text
-    .replace(/([A-Za-z])\s*<br\s*\/?>\s*([A-Za-z])/g, '$1$2')
-    .replace(/([A-Za-z])\r?\n\s*([A-Za-z])/g, '$1$2');
+  const cleaned = text
+    // Remove invisible break characters that can split words.
+    .replace(/[\u00ad\u200b\u200c\u200d\u2060\ufeff]/g, '');
+
+  return cleaned
+    .replace(/([A-Za-z])\s*<br\s*\/?>\s*([A-Za-z])/gi, '$1$2')
+    // Join words split by line separators/newlines.
+    .replace(/([A-Za-z])[\r\n\u2028\u2029]+\s*([A-Za-z])/g, '$1$2')
+    // Join words split by tabs between letters.
+    .replace(/([A-Za-z])\t+\s*([A-Za-z])/g, '$1$2');
 }
 
 export function normalizeParagraphText(text: string): string {
